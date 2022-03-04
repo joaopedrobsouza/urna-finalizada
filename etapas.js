4120d497d5da2ead60989b2e2ae5ec5e72037237
joaopedrobsouza <100635048+joaopedrobsouza@users.noreply.github.com>
2022-03-04 20:03:53 -0300

    Projeto da Urna Eletrônica finalizado - HTML | CSS | JAVASCRIPT

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Arnaldo Minelvino',
+                partido: 'Liberais',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Fatinha Marques',
+                partido: 'Conservadores',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Louro da Cruz',
+                partido: 'ABC',
+                vice: 'Louro da Picape',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zezé de Camargo',
+                partido: 'QWERTY',
+                vice: 'E Luciano',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
